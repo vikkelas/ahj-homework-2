c4e0a858121d59b8707175cf7e1fd85535a33488
Vikkelas <bass89@mail.ru>
2022-01-25 01:49:26 +0300

    home work 2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Webpack5
+https://ci.appveyor.com/api/projects/status/ea3ifeq4ss4ndxdr/branch/main?svg=true
+
+[Информация о релизе Webpack 5](https://webpack.js.org/blog/2020-10-10-webpack-5-release/)
+
+[Migration Guide](https://webpack.js.org/migrate/5/)
---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+// TODO: write code here
+let index = null;
+
+function randomPosition() {
+  const arrCell = [...document.querySelectorAll('.box-cell')];
+  const activeCell = Math.floor(Math.random() * arrCell.length);
+  if (index === null) {
+    index = Math.floor(Math.random() * arrCell.length);
+    arrCell[index].classList.add('box__cell-active');
+  } else if (index === activeCell) {
+    arrCell[index].classList.remove('box__cell-active');
+    index = Math.floor(Math.random() * arrCell.length);
+    arrCell[index].classList.add('box__cell-active');
+  } else {
+    arrCell[index].classList.remove('box__cell-active');
+    index = activeCell;
+    arrCell[index].classList.add('box__cell-active');
+  }
+}
+
+setInterval(randomPosition, 2000);
